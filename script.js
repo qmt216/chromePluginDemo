9575882716c40f4a9682ffaa600679748520a4e6
qinmingtao <qinmingtao@kaolakuaishou.com>
2022-03-25 19:03:23 +0800

    do

--- script.js
+++ script.js
@@ -1,58 +1,66 @@
 $('input[name=user]').val('17600051750');
 $('input[name=psd]').val('YXY20220112ljh02');
 $('#TencentCaptcha').click();
+//当前token
+var token = "";
+//当前是否需要执行任务的标识
+var taskTag = true;
+//每天2点重置task标识
+setInterval(function () {
+    var refreshHours = new Date().getHours();
+    var refreshMin = new Date().getMinutes();
+    var refreshSec = new Date().getSeconds();
+    //每天2点
+    if (refreshHours === 14 && refreshMin === 0 && refreshSec === 0) {
+        taskTag = true;
+    }
+}, 1000);
+//刷新页面 10分钟刷新一次
+setInterval(refreshPage, 1000 * 60 * 10);
 
-var preToken = $('input[name=jwtToken][type=hidden]').val();
-console.log('preToken', preToken);
-//刷新页面
-setTimeout(refashPage, 1000 * 3);
-
-function refashPage() {
-  let afterToken = $('input[name=jwtToken][type=hidden]').val();
-  console.log('afterToken', afterToken);
-  if (preToken === afterToken || afterToken != '') {
-    // sendToken(afterToken);
-  }
-  //跳转我的利润页
-  $('#profit-agent').find('.J_menuItem').eq(0).click();
-
-  //跳转报表下载
-  setTimeout(()=>{
-    $('#export-down').find('.J_menuItem').eq(0).click();
-    setTimeout(()=>{
-      $(window.frames["iframe13-17"].document).find("#data_table .RoleOfA.table-btn-primary").eq(0).click();
-    }, 10000);
-  },10000);
-
-  // setTimeout(()=>{
-  //   //触发下载
-  //   $(window.frames["iframe10"].document).find("#toolbar .ul-list .item").eq(0).click();
-  //   //跳转报表下载
-  //   setTimeout(()=>{
-  //     $('#export-down').find('.J_menuItem').eq(0).click();
-  //     setTimeout(()=>{
-  //       $(window.frames["iframe13-17"].document).find("#data_table .RoleOfA.table-btn-primary").eq(0).click();
-  //     }, 10000);
-  //   },10000);
-  // }, 10000);
+function refreshPage() {
+    let newToken = $('input[name=jwtToken][type=hidden]').val();
+    console.log('newToken', newToken, token);
+    if (token !== newToken) {
+        token = newToken;
+        sendToken(newToken);
+    }
+    if (taskTag) {
+        //跳转我的利润页
+        $('#profit-agent').find('.J_menuItem').eq(0).click();
+        setTimeout(() => {
+            //触发下载
+            $(window.frames["iframe10"].document).find("#toolbar .ul-list .item").eq(0).click();
+            //跳转报表下载
+            setTimeout(() => {
+                $('#export-down').find('.J_menuItem').eq(0).click();
+                setTimeout(() => {
+                    debugger;
+                    $(window.frames["iframe13-17"].document).find("#data_table td:contains('posp交易流水导出')")
+                        .siblings('.RoleOfA.table-btn-primary').eq(0).click();
+                    taskTag = false;
+                }, 10000);
+            }, 10000);
+        }, 10000);
+    }
 }
 
 function sendToken(token) {
-  console.log('token==', token);
-  $.ajax({
-    type: 'post',
-    url: 'https://api-test-haike.kaolakuaishou.cn' + '/home/getRecommendCount',
-    data: {
-      userId: token,
-    },
-    dataType: 'text',
-    success: function (data) {
-      console.log('data==', data);
-    },
-    error: function (jqXHR) {
-      // alert(jqXHR);
-    },
-  });
+    console.log('token==', token);
+    $.ajax({
+        type: 'post',
+        url: 'https://api-test-haike.kaolakuaishou.cn' + '/home/getRecommendCount',
+        data: {
+            userId: token,
+        },
+        dataType: 'text',
+        success: function (data) {
+            console.log('data==', data);
+        },
+        error: function (jqXHR) {
+            // alert(jqXHR);
+        },
+    });
 }
 
 // $('button').eq(0).click()=function(){
@@ -65,7 +73,6 @@ function sendToken(token) {
 
 //列表点击触发下载按钮
 // $('button').eq(0).click();
-
 
 
 // ===================
